9eb82478f6c5189cb6caf9127d2f254bc51e98ba
gfleetwood <7785756+gfleetwood@users.noreply.github.com>
2024-01-16 19:49:47 -0500

    Update app.py
    Add explicit citing of sources

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import random
 import openai
 import time
 from os import environ
-
 from canopy.knowledge_base import KnowledgeBase
 from canopy.tokenizer import Tokenizer
 from canopy.models.data_models import Query
@@ -19,8 +18,7 @@ def get_prompt_output(question, transcript):
     
     The text below is the knowledge you need to answer the user's question. 
     Only use information from what is given below, and give code examples when appropriate. 
-    Please cite the sources.
-    If the question is not related to the sources, reply "I don't know":
+    Always cite the included sources. If the question is not related to the sources, reply "I don't know":
     
     {}
     """
@@ -37,6 +35,15 @@ def get_prompt_output(question, transcript):
     
     return(prompt_output)
 
+def get_db_content(question):
+
+    result = context_engine.query([Query(text = question, top_k = 5)], max_context_tokens = 512)
+    query = result.dict()["content"][0]['query']
+    snippets = "\n\n".join(["source: {}\n\ntext: {}".format(x["source"], x["text"]) for x in result.dict()["content"][0]['snippets']])
+    sources = "Sources:\n\n" + "\n\n".join([x["source"] for x in result.dict()["content"][0]['snippets']])
+    
+    return((query, snippets, sources))
+
 st.title("Modal Docs Bot Chat")
 st.text("Ask questions about Modal Lab's documentation: https://modal.com/docs/examples")
 
@@ -44,14 +51,6 @@ Tokenizer.initialize()
 kb = KnowledgeBase(index_name = environ["INDEX_NAME"])
 kb.connect()
 context_engine = ContextEngine(kb)
-
-def get_db_content(question):
-
-    result = context_engine.query([Query(text = question, top_k = 5)], max_context_tokens = 512)
-    query = result.dict()["content"][0]['query']
-    snippets = "\n\n".join(["source: {}\n\ntext: {}".format(x["source"], x["text"]) for x in result.dict()["content"][0]['snippets']])
-    
-    return((query, snippets))
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -72,8 +71,8 @@ if prompt := st.chat_input("I'm your modal bot please ask me questions"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
-        query, snippets = get_db_content(prompt)
-        full_response = get_prompt_output(query, snippets)
+        query, snippets, sources = get_db_content(prompt)
+        full_response = get_prompt_output(query, snippets) + "\n\n" + sources
         message_placeholder.markdown(full_response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
